0d5a1f2c2ca56e4edd887fd840753d8ee96289df
BK3000gamer <bk3000gamer@gmail.com>
2026-05-20 20:33:43 +1000

    give player card every 10 sec

--- Commons/Singletons/global_timer.gd
+++ Commons/Singletons/global_timer.gd
@@ -6,13 +6,24 @@ var m: int = 0
 var s: int = 0
 var timer: Timer
 var time: int
+var pickup_timer: int = 0
+var player: Player
 
 func _ready() -> void:
 	process_mode = Node.PROCESS_MODE_ALWAYS
+	start_game()
+	while player == null:
+		player = get_tree().get_first_node_in_group("local_player")
+		if player == null:
+			await get_tree().process_frame
 
 func _process(delta: float) -> void:
 	m = int(time / 60)
 	s = time - m * 60
+	
+	if pickup_timer == 10:
+		pickup_timer = 0
+		player.pickup_card(distribute_cards())
 	
 	if time == 0:
 		stop_game()
@@ -33,3 +44,10 @@ func stop_game() -> void:
 
 func timeout():
 	time -= 1
+	pickup_timer += 1
+
+func distribute_cards() -> String:
+	var card_id: Array[String] = \
+	["Dash", "Speed Boost", "Stomp", "Updraft", "Arcane"]
+	var chosen_card: String = card_id.pick_random()
+	return chosen_card
